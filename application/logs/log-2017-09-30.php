3d07516d1ae3cbf5b2d6050e14a08511042c55e1
Jan-Willem Huisman <jhuisman2@gmail.com>
2017-10-03 20:09:02 +0200

    Added CodeIgniter

---
+++ application/logs/log-2017-09-30.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-09-30 19:10:25 --> Config Class Initialized
+INFO - 2017-09-30 19:10:25 --> Hooks Class Initialized
+DEBUG - 2017-09-30 19:10:25 --> UTF-8 Support Enabled
+INFO - 2017-09-30 19:10:25 --> Utf8 Class Initialized
+INFO - 2017-09-30 19:10:25 --> URI Class Initialized
+DEBUG - 2017-09-30 19:10:25 --> No URI present. Default controller set.
+INFO - 2017-09-30 19:10:25 --> Router Class Initialized
+INFO - 2017-09-30 19:10:25 --> Output Class Initialized
+INFO - 2017-09-30 19:10:25 --> Security Class Initialized
+DEBUG - 2017-09-30 19:10:25 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-09-30 19:10:25 --> Input Class Initialized
+INFO - 2017-09-30 19:10:25 --> Language Class Initialized
+INFO - 2017-09-30 19:10:25 --> Loader Class Initialized
+INFO - 2017-09-30 19:10:25 --> Controller Class Initialized
+INFO - 2017-09-30 19:10:25 --> File loaded: C:\Program Files (x86)\Ampps\www\codeuur.dev\application\views\welcome_message.php
+INFO - 2017-09-30 19:10:25 --> Final output sent to browser
+DEBUG - 2017-09-30 19:10:25 --> Total execution time: 0.0291
