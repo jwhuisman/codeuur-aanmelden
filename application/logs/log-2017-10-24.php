9bddc90104315d8e3849e6724a8bc5c5443871d9
Jan-Willem Huisman <jhuisman2@gmail.com>
2017-10-24 19:44:19 +0200

    Logs

---
+++ application/logs/log-2017-10-24.php
@@ -0,0 +1,87 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-10-24 19:40:37 --> Config Class Initialized
+INFO - 2017-10-24 19:40:37 --> Hooks Class Initialized
+DEBUG - 2017-10-24 19:40:37 --> UTF-8 Support Enabled
+INFO - 2017-10-24 19:40:37 --> Utf8 Class Initialized
+INFO - 2017-10-24 19:40:37 --> URI Class Initialized
+DEBUG - 2017-10-24 19:40:37 --> No URI present. Default controller set.
+INFO - 2017-10-24 19:40:37 --> Router Class Initialized
+INFO - 2017-10-24 19:40:37 --> Output Class Initialized
+INFO - 2017-10-24 19:40:37 --> Security Class Initialized
+DEBUG - 2017-10-24 19:40:37 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-10-24 19:40:37 --> Input Class Initialized
+INFO - 2017-10-24 19:40:37 --> Language Class Initialized
+INFO - 2017-10-24 19:40:37 --> Loader Class Initialized
+INFO - 2017-10-24 19:40:37 --> Controller Class Initialized
+INFO - 2017-10-24 19:40:37 --> File loaded: C:\Program Files (x86)\Ampps\www\codeuur.dev\application\views\welcome_message.php
+INFO - 2017-10-24 19:40:37 --> Final output sent to browser
+DEBUG - 2017-10-24 19:40:37 --> Total execution time: 0.0326
+INFO - 2017-10-24 19:40:38 --> Config Class Initialized
+INFO - 2017-10-24 19:40:38 --> Hooks Class Initialized
+DEBUG - 2017-10-24 19:40:38 --> UTF-8 Support Enabled
+INFO - 2017-10-24 19:40:38 --> Utf8 Class Initialized
+INFO - 2017-10-24 19:40:38 --> URI Class Initialized
+DEBUG - 2017-10-24 19:40:38 --> No URI present. Default controller set.
+INFO - 2017-10-24 19:40:38 --> Router Class Initialized
+INFO - 2017-10-24 19:40:38 --> Output Class Initialized
+INFO - 2017-10-24 19:40:38 --> Security Class Initialized
+DEBUG - 2017-10-24 19:40:38 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-10-24 19:40:38 --> Input Class Initialized
+INFO - 2017-10-24 19:40:38 --> Language Class Initialized
+INFO - 2017-10-24 19:40:38 --> Loader Class Initialized
+INFO - 2017-10-24 19:40:38 --> Controller Class Initialized
+INFO - 2017-10-24 19:40:38 --> File loaded: C:\Program Files (x86)\Ampps\www\codeuur.dev\application\views\welcome_message.php
+INFO - 2017-10-24 19:40:38 --> Final output sent to browser
+DEBUG - 2017-10-24 19:40:38 --> Total execution time: 0.0166
+INFO - 2017-10-24 19:40:39 --> Config Class Initialized
+INFO - 2017-10-24 19:40:39 --> Hooks Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> UTF-8 Support Enabled
+INFO - 2017-10-24 19:40:39 --> Utf8 Class Initialized
+INFO - 2017-10-24 19:40:39 --> URI Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> No URI present. Default controller set.
+INFO - 2017-10-24 19:40:39 --> Router Class Initialized
+INFO - 2017-10-24 19:40:39 --> Output Class Initialized
+INFO - 2017-10-24 19:40:39 --> Security Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-10-24 19:40:39 --> Input Class Initialized
+INFO - 2017-10-24 19:40:39 --> Language Class Initialized
+INFO - 2017-10-24 19:40:39 --> Loader Class Initialized
+INFO - 2017-10-24 19:40:39 --> Controller Class Initialized
+INFO - 2017-10-24 19:40:39 --> File loaded: C:\Program Files (x86)\Ampps\www\codeuur.dev\application\views\welcome_message.php
+INFO - 2017-10-24 19:40:39 --> Final output sent to browser
+DEBUG - 2017-10-24 19:40:39 --> Total execution time: 0.0221
+INFO - 2017-10-24 19:40:39 --> Config Class Initialized
+INFO - 2017-10-24 19:40:39 --> Hooks Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> UTF-8 Support Enabled
+INFO - 2017-10-24 19:40:39 --> Utf8 Class Initialized
+INFO - 2017-10-24 19:40:39 --> URI Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> No URI present. Default controller set.
+INFO - 2017-10-24 19:40:39 --> Router Class Initialized
+INFO - 2017-10-24 19:40:39 --> Output Class Initialized
+INFO - 2017-10-24 19:40:39 --> Security Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-10-24 19:40:39 --> Input Class Initialized
+INFO - 2017-10-24 19:40:39 --> Language Class Initialized
+INFO - 2017-10-24 19:40:39 --> Loader Class Initialized
+INFO - 2017-10-24 19:40:39 --> Controller Class Initialized
+INFO - 2017-10-24 19:40:39 --> File loaded: C:\Program Files (x86)\Ampps\www\codeuur.dev\application\views\welcome_message.php
+INFO - 2017-10-24 19:40:39 --> Final output sent to browser
+DEBUG - 2017-10-24 19:40:39 --> Total execution time: 0.0163
+INFO - 2017-10-24 19:40:39 --> Config Class Initialized
+INFO - 2017-10-24 19:40:39 --> Hooks Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> UTF-8 Support Enabled
+INFO - 2017-10-24 19:40:39 --> Utf8 Class Initialized
+INFO - 2017-10-24 19:40:39 --> URI Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> No URI present. Default controller set.
+INFO - 2017-10-24 19:40:39 --> Router Class Initialized
+INFO - 2017-10-24 19:40:39 --> Output Class Initialized
+INFO - 2017-10-24 19:40:39 --> Security Class Initialized
+DEBUG - 2017-10-24 19:40:39 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-10-24 19:40:39 --> Input Class Initialized
+INFO - 2017-10-24 19:40:39 --> Language Class Initialized
+INFO - 2017-10-24 19:40:39 --> Loader Class Initialized
+INFO - 2017-10-24 19:40:39 --> Controller Class Initialized
+INFO - 2017-10-24 19:40:39 --> File loaded: C:\Program Files (x86)\Ampps\www\codeuur.dev\application\views\welcome_message.php
+INFO - 2017-10-24 19:40:39 --> Final output sent to browser
+DEBUG - 2017-10-24 19:40:39 --> Total execution time: 0.0216
